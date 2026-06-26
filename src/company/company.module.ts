b67913863a9124681929d1293bde7000751f78d2
Ítalo Lima <contatoitallo@hotmail.com>
2026-06-26 12:52:00 -0300

    refactor(company): religa o modulo nas portas, casos de uso e controller novo

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,18 +1,48 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CompanyService } from './company.service';
-import { CompanyController } from './company.controller';
 import { Company } from './entities/company.entity';
 import { User } from '../user/entities/user.entity';
+import { UserCompany } from '@/shared/entities/user-company.entity';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
-import { UserService } from '@/user/user.service';
-import { UserCompany } from '@/shared/entities/user-company.entity';
+import { CompanyController } from './infrastructure/http/company.controller';
+import { TypeOrmCompanyRepository } from './infrastructure/persistence/typeorm-company.repository';
+import { UserGatewayAdapter } from './infrastructure/user/user.gateway.adapter';
+import { COMPANY_REPOSITORY } from './domain/ports/company.repository';
+import { USER_GATEWAY } from './domain/ports/user-gateway';
+import { GetCompanyByIdUseCase } from './application/get-company-by-id.usecase';
+import { FindOneCompanyUseCase } from './application/find-one-company.usecase';
+import { FindAllCompaniesUseCase } from './application/find-all-companies.usecase';
+import { FindCompaniesByIdUseCase } from './application/find-companies-by-id.usecase';
+import { CreateCompanyUseCase } from './application/create-company.usecase';
+import { CreateCompanyByManagerUseCase } from './application/create-company-by-manager.usecase';
+import { UpdateCompanyUseCase } from './application/update-company.usecase';
+import { RemoveCompanyUseCase } from './application/remove-company.usecase';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserCompany, Company, User]), forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    TypeOrmModule.forFeature([UserCompany, Company, User]),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [CompanyController],
-  providers: [CompanyService, UserService],
-  exports: [TypeOrmModule.forFeature([Company]), TypeOrmModule, CompanyService],
+  providers: [
+    { provide: COMPANY_REPOSITORY, useClass: TypeOrmCompanyRepository },
+    { provide: USER_GATEWAY, useClass: UserGatewayAdapter },
+    GetCompanyByIdUseCase,
+    FindOneCompanyUseCase,
+    FindAllCompaniesUseCase,
+    FindCompaniesByIdUseCase,
+    CreateCompanyUseCase,
+    CreateCompanyByManagerUseCase,
+    UpdateCompanyUseCase,
+    RemoveCompanyUseCase,
+  ],
+  exports: [
+    GetCompanyByIdUseCase,
+    FindOneCompanyUseCase,
+    FindCompaniesByIdUseCase,
+    TypeOrmModule,
+  ],
 })
 export class CompanyModule {}
