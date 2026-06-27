248a7ef9ea23adadea7dfe70bc0e30c4c3d634b4
Ítalo Lima <contatoitallo@hotmail.com>
2026-06-26 23:00:44 -0300

    refactor(ticket): qrcode gateway usa GenerateQrCodeUseCase no lugar do QrcodeService removido

--- src/ticket/infrastructure/qrcode/qrcode.gateway.adapter.ts
+++ src/ticket/infrastructure/qrcode/qrcode.gateway.adapter.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { QrcodeGateway, TicketSnapshot } from '../../domain/ports/qrcode-gateway';
-import { QrcodeService } from '../../../qrcode/qrcode.service';
-import { Ticket } from '../../entities/ticket.entity';
+import { GenerateQrCodeUseCase } from '../../../qrcode/application/generate-qr-code.usecase';
 
 @Injectable()
 export class QrcodeGatewayAdapter implements QrcodeGateway {
-  constructor(private readonly qrcodeService: QrcodeService) {}
+  constructor(private readonly generateQrCode: GenerateQrCodeUseCase) {}
 
   async generateForTicket(ticket: TicketSnapshot): Promise<string> {
-    const result = await this.qrcodeService.generateQrCode({
+    const result = await this.generateQrCode.execute({
       id: ticket.id,
       ticketNumber: ticket.ticketNumber,
       checkInTime: ticket.checkInTime,
-    } as Ticket);
+    });
     return result.qrCode;
   }
 }
