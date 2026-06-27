983446bfddec9147c75885ec7b6e8115d606025b
Ítalo Lima <contatoitallo@hotmail.com>
2026-06-26 22:50:23 -0300

    refactor(ticket): religa o modulo nas portas, casos de uso e controller novo

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -1,19 +1,44 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { TicketService } from './ticket.service';
-import { TicketController } from './ticket.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity';
 import { AuthModule } from '../auth/auth.module';
-import { UserModule } from '../user/user.module';
 import { ApiKeyModule } from '../api_key/api-key.module';
-import { UserService } from '@/user/user.service';
-import { CompanyModule } from '@/company/company.module';
 import { QrcodeModule } from '@/qrcode/qrcode.module';
+import { TicketController } from './infrastructure/http/ticket.controller';
+import { TypeOrmTicketRepository } from './infrastructure/persistence/typeorm-ticket.repository';
+import { ApiKeyGatewayAdapter } from './infrastructure/api-key/api-key.gateway.adapter';
+import { QrcodeGatewayAdapter } from './infrastructure/qrcode/qrcode.gateway.adapter';
+import { TICKET_REPOSITORY } from './domain/ports/ticket.repository';
+import { API_KEY_GATEWAY } from './domain/ports/api-key-gateway';
+import { QRCODE_GATEWAY } from './domain/ports/qrcode-gateway';
+import { ResolveApiKeyCompanyUseCase } from './application/resolve-api-key-company.usecase';
+import { CreateTicketUseCase } from './application/create-ticket.usecase';
+import { FindAllTicketsUseCase } from './application/find-all-tickets.usecase';
+import { FindTicketByIdUseCase } from './application/find-ticket-by-id.usecase';
+import { FindTicketByNumberUseCase } from './application/find-ticket-by-number.usecase';
+import { UpdateTicketUseCase } from './application/update-ticket.usecase';
+import { RemoveTicketUseCase } from './application/remove-ticket.usecase';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket]), forwardRef(() => AuthModule), forwardRef(() => UserModule), forwardRef(() => ApiKeyModule), forwardRef(() => CompanyModule), forwardRef(() => QrcodeModule)],
-  providers: [TicketService, UserService],
-  exports: [TicketService],
+  imports: [
+    TypeOrmModule.forFeature([Ticket]),
+    forwardRef(() => AuthModule),
+    forwardRef(() => ApiKeyModule),
+    forwardRef(() => QrcodeModule),
+  ],
   controllers: [TicketController],
+  providers: [
+    { provide: TICKET_REPOSITORY, useClass: TypeOrmTicketRepository },
+    { provide: API_KEY_GATEWAY, useClass: ApiKeyGatewayAdapter },
+    { provide: QRCODE_GATEWAY, useClass: QrcodeGatewayAdapter },
+    ResolveApiKeyCompanyUseCase,
+    CreateTicketUseCase,
+    FindAllTicketsUseCase,
+    FindTicketByIdUseCase,
+    FindTicketByNumberUseCase,
+    UpdateTicketUseCase,
+    RemoveTicketUseCase,
+  ],
+  exports: [FindTicketByNumberUseCase, TypeOrmModule],
 })
 export class TicketModule {}
